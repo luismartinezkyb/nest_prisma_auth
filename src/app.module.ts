82644f99b925522d060ae0a3b19c016731053cbe
luismartinezkyb <18030545@itcelaya.edu.mx>
2023-12-08 08:16:16 -0600

    (feat): serve public static folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,20 @@ import { ProductsModule } from './products/products.module';
 import { DbModule } from './db/db.module';
 import { CommonModule } from './common/common.module';
 import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ProductsModule, DbModule, CommonModule, FilesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ProductsModule,
+    DbModule,
+    CommonModule,
+    FilesModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
